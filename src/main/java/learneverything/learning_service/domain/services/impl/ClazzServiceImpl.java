b6aae335f2e5daea6e60ad1305f9da43c3b38d2d
anhduc0523 <anhducntt2002@gmail.com>
2025-05-15 16:15:53 +0700

    feat: update api get clazz

--- src/main/java/learneverything/learning_service/domain/services/impl/ClazzServiceImpl.java
+++ src/main/java/learneverything/learning_service/domain/services/impl/ClazzServiceImpl.java
@@ -2,26 +2,42 @@ package learneverything.learning_service.domain.services.impl;
 
 import learneverything.learning_service.application.exceptions.BaseException;
 import learneverything.learning_service.application.exceptions.Error;
+import learneverything.learning_service.database.entities.ChapterEntity;
 import learneverything.learning_service.database.entities.ClazzEntity;
+import learneverything.learning_service.database.repositories.ChapterRepository;
 import learneverything.learning_service.database.repositories.ClazzRepository;
+import learneverything.learning_service.domain.dtos.chapter.ChapterDTO;
 import learneverything.learning_service.domain.dtos.clazz.ClazzDTO;
 import learneverything.learning_service.domain.dtos.clazz.CreateClazzRequestDTO;
 import learneverything.learning_service.domain.dtos.clazz.UpdateClazzRequestDTO;
+import learneverything.learning_service.domain.mappers.ChapterMapper;
 import learneverything.learning_service.domain.mappers.ClazzMapper;
 import learneverything.learning_service.domain.services.ClazzService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ClazzServiceImpl implements ClazzService {
     private final ClazzRepository clazzRepository;
     private final ClazzMapper clazzMapper;
+    private final ChapterRepository chapterRepository;
+    private final ChapterMapper chapterMapper;
 
     @Override
     public ClazzDTO get(Integer id) {
         ClazzEntity clazz = clazzRepository.findById(id).orElseThrow(()->new BaseException(Error.NOT_FOUND_LESSON,id.toString()));
-        return clazzMapper.toDTO(clazz);
+        ClazzDTO clazzDTO = clazzMapper.toDTO(clazz);
+
+        List<ChapterEntity> chapterEntities = chapterRepository.findActiveChaptersOfClazz(id);
+        if (chapterEntities.size() > 0){
+            List<ChapterDTO> chapterDTOS = chapterEntities.stream().map(chapterMapper::toDTO).toList();
+            clazzDTO.setListChapter(chapterDTOS);
+        }
+
+        return clazzDTO;
     }
 
     @Override
